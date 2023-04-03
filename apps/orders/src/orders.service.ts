3136719dd306ff602dc88f226dc56658d3bbb964
Kaung Myat Han <arkarhtethan@gmail.com>
2023-04-03 20:25:19 +0630

    create and get order routes added

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private readonly ordersRepository: OrdersRepository) { }
+
+  async createOrder (request: CreateOrderRequest) {
+    try {
+      return this.ordersRepository.create(request);
+    } catch (error) {
+
+    }
+  }
+
+  async getOrders () {
+    return this.ordersRepository.find({});
   }
 }
